f671b80f32a201a7b3f80b0f92edf7816308f73d
unknown <soumya.ray2472@gmail.com>
2024-03-24 15:06:28 +0530

    Changed the header message just for the sake of it

--- Portfolio Project/frontend/src/components/HeaderSection.jsx
+++ Portfolio Project/frontend/src/components/HeaderSection.jsx
@@ -4,7 +4,7 @@ export const HeaderSection = () => {
         <>
             <div className="bg-red-500 h-52 flex items-center justify-center mt-2 mr-2 ml-2 rounded-lg">
                 <div className="">
-                    <h1>Header Image</h1>
+                    <h1>Header Image or a Heartwarming Welcome Message</h1>
                 </div>
             </div>
         </>
