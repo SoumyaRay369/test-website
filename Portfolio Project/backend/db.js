1edfa345e4a72caeaf7fb2784a533e15dbe66fd0
unknown <soumya.ray2472@gmail.com>
2024-03-26 14:14:16 +0530

    Fixed the deprecated mongoose connection string parameters

--- Portfolio Project/backend/db.js
+++ Portfolio Project/backend/db.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://soumyaray2472:soumyaray2472@cluster0.otvx3fa.mongodb.net/portfolio_project?retryWrites=true&w=majority&appName=Cluster0", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-})
+mongoose.connect("mongodb+srv://soumyaray2472:soumyaray2472@cluster0.otvx3fa.mongodb.net/portfolio_project?retryWrites=true&w=majority&appName=Cluster0")
 
 const User = mongoose.model('newUsersTable', {
     username: String,
