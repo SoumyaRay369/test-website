1b506a480d44cfa74b3535fc52c5f425a3c21d92
Soumya_Ray <soumya.ray2472@gmail.com>
2024-09-13 14:12:26 +0530

    Made the website more SEO Optimised

--- Portfolio Project/frontend/src/components/ConnectWithMe.jsx
+++ Portfolio Project/frontend/src/components/ConnectWithMe.jsx
@@ -26,7 +26,7 @@ export const ConnectWithMe = () => {
     }
     return (
         <>
-            <div className="text-center md:text-xl text-lg mt-10">Connect With Me</div>
+            <div className="text-center text-3xl font-playfair mt-10">Connect With Me</div>
             <div className="ml-10 mr-4 md:ml-96 items-center">
                 <form onSubmit={detailSubmission} className="flex flex-col gap-y-3 mt-4 ">
                     <input type="text" value={userName} onChange={(e) => changeUserName(e.target.value)} placeholder="Name" className="border-2 border-black rounded-md p-3 focus:border-blue-500 w-full md:w-1/2" />
