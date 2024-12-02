9f101a00aaf66c9b0cb5c77c527c4cc37f39041f
Soumya_Ray <soumya.ray2472@gmail.com>
2024-12-02 13:00:30 +0530

    Changed Software Engineering to be default skill

--- Portfolio Project/frontend/src/components/FieldOfExpertise.jsx
+++ Portfolio Project/frontend/src/components/FieldOfExpertise.jsx
@@ -1,8 +1,8 @@
-import {Analytics} from './Analytics'
+import { Analytics } from './Analytics'
 import { SDE } from './SDE'
 import { useState } from 'react';
 export const FieldOfExpertise = () => {
-    const [selectedField, setSelectedField] = useState('analytics');
+    const [selectedField, setSelectedField] = useState('sde');
 
     const renderField = () => {
         if (selectedField === 'analytics') {
@@ -15,12 +15,14 @@ export const FieldOfExpertise = () => {
     return (
         <>
             <div className='mt-10 flex justify-center gap-x-5'>
-                <button onClick={() => setSelectedField('analytics')} className='bg-blue-400 rounded-md p-2 text-white'>Data Analytics</button>
-                <button onClick={() => setSelectedField('sde')} className='bg-blue-400 rounded-md p-2 text-white'>Software Engineering</button>
+                
+                <button onClick={() => setSelectedField('sde')} className='bg-blue-400 hover:bg-blue-500 rounded-xl p-3 text-white'>Software Engineering</button>
+                <button onClick={() => setSelectedField('analytics')} className='bg-blue-400 hover:bg-blue-500 rounded-xl p-2 text-white'>Data Analytics</button>
+
             </div>
             {renderField()}
-            
-            
+
+
         </>
     )
 } 
