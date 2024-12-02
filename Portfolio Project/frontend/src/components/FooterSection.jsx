8df249fcf9f2774120f26f1fc4efc4632abb7542
Soumya_Ray <soumya.ray2472@gmail.com>
2024-12-02 12:52:13 +0530

    Deleted the address and changed the footer font

--- Portfolio Project/frontend/src/components/FooterSection.jsx
+++ Portfolio Project/frontend/src/components/FooterSection.jsx
@@ -1,11 +1,10 @@
 export const FooterSection = () => {
     return (
         <>
-            <div className="bg-black flex flex-col gap-y-2 pt-5 pl-5 md:pl-2 text-white  mt-10 text-sm md:text-md">
+            <div className="bg-black font-garamond flex flex-col gap-y-2 pt-5 pl-5 md:pl-2 text-white  mt-10 text-md">
                 
                     <div>soumya.ray2472@gmail.com </div>
                     <div>isoumya.ray@gmail.com</div>
-                    <div>37/5, South Road, Jadavpur Santoshpur, Kolkata - 70075</div>
                     <div>soumyaece.stud@nita.ac.in</div>
                     <div>https://github.com/SoumyaRay369/</div>
                     <div>https://www.linkedin.com/in/soumya-ray-281932224/</div>
@@ -14,7 +13,7 @@ export const FooterSection = () => {
             </div>
 
             <div>
-                <div className="bg-black font-playfair text-white text-center text-sm md:text-md py-2">
+                <div className="bg-black font-playfair text-white text-center text-sm md:text-md pt-4">
                     © 2021 Soumya Ray. All Rights Reserved.
                 </div>
             </div>
