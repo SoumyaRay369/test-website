c65efdbe4e6ab1153422aaaa1b7330f23fb7ffe4
Soumya_Ray <soumya.ray2472@gmail.com>
2024-12-03 15:02:27 +0530

    Fixed the Skills Section Styling

--- Portfolio Project/frontend/src/components/SDE.jsx
+++ Portfolio Project/frontend/src/components/SDE.jsx
@@ -11,36 +11,41 @@ import googleCertificate from '../assets/google_analytics.png';
 import dukaanappClone from '../assets/dukaan_app_clone.png';
 import newsletterSignin from '../assets/newsletter_signin.png';
 import videoChatApplication from '../assets/videoChatApplication.jpg';
+import googleAIEssentials from '../assets/google_ai_essentials.jpg';
 export const SDE = () => {
     return (
         <>
             <div className="mt-10 text-center text-3xl font-playfair">Software Engineering Skills</div>
             <div className="mt-8 mr-10 ml-10 text-xs md:text-lg">
                 <div className="flex justify-between overflow-x-auto gap-x-8">
+
+                    
                     <div className="flex flex-col items-center flex-shrink-0">
-                        <div><img src={gitLogo} alt="" className='w-28 md:w-36 h-14 md:h-32' /></div>
+                        <div><img src={gitLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">Git</div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div><img src={javascriptLogo} alt="" className='w-40 md:w-36 h-14 md:h-32' /></div>
+                    <div className="flex flex-col items-center flex-shrink-0">
+                        <div><img src={javascriptLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">JavaScript</div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div><img src={typescriptLogo} alt="" className='w-40 md:w-36 h-14 md:h-32' /></div>
+                    <div className="flex flex-col items-center flex-shrink-0">
+                        <div><img src={typescriptLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">TypeScript</div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div><img src={webrtcLogo} alt="" className='w-40 md:w-36 h-14 md:h-32' /></div>
+                    <div className="flex flex-col items-center flex-shrink-0">
+                        <div><img src={webrtcLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">WebRTC</div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div><img src={nodejsLogo} alt="" className='w-40 md:w-36 h-14 md:h-32' /></div>
+                    <div className="flex flex-col items-center flex-shrink-0">
+                        <div><img src={nodejsLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">Node.js</div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div><img src={mongodbLogo} alt="" className='w-40 md:w-36 h-14 md:h-32' /></div>
+                    <div className="flex flex-col items-center flex-shrink-0">
+                        <div><img src={mongodbLogo} alt="" className='w-20 md:w-36 h-20 md:h-32 rounded-md' /></div>
                         <div className="font-garamond">MongoDB</div>
                     </div>
+
+
                 </div>
             </div>
 
@@ -71,22 +76,27 @@ export const SDE = () => {
                 <div className="flex justify-between overflow-x-auto gap-x-8">
                     
                     <div className="flex flex-col items-center flex-shrink-0 ">
-                        <div><img src={googleCertificate} alt="" className='rounded-md w-50 h-20 ' /></div>
+                        <div><img src={googleAIEssentials} alt="" className='rounded-md w-50 md:w-60 h-20 md:h-40 ' /></div>
+                        <div className="font-garamond">Google AI Essentials Certificate</div>
+                    </div>
+
+                    <div className="flex flex-col items-center flex-shrink-0 ">
+                        <div><img src={googleCertificate} alt="" className='rounded-md w-50 md:w-60 h-20 md:h-40 ' /></div>
                         <div className="font-garamond">Google Data Analytics Certificate</div>
                     </div>
 
                     <div className="flex flex-col items-center flex-shrink-0">
-                        <div><img src={tableauCertificate} alt="" className='rounded-md w-50 h-20 ' /></div>
+                        <div><img src={tableauCertificate} alt="" className='rounded-md w-50 md:w-60 h-20 md:h-40 ' /></div>
                         <div className="font-garamond">Udemy Tableau Certificate</div>
                     </div>
 
                     <div className="flex flex-col items-center flex-shrink-0">
-                        <div><img src={pythonCertificate} alt="" className='rounded-md w-50 h-20 ' /></div>
+                        <div><img src={pythonCertificate} alt="" className='rounded-md w-50 md:w-60 h-20 md:h-40 ' /></div>
                         <div className="font-garamond">Udemy Python Certificate</div>
                     </div>
 
                     <div className="flex flex-col items-center flex-shrink-0">
-                        <div><img src={sqlCertificate} alt="" className='rounded-md w-50 h-20 ' /></div>
+                        <div><img src={sqlCertificate} alt="" className='rounded-md w-50 md:w-60 h-20 md:h-40 ' /></div>
                         <div className="font-garamond">Udemy SQL Certificate</div>
                     </div>
 
